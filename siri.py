385d07db4d355bedef2326b98017201f06088fd0
sireesha98 <41771257+sireesha98@users.noreply.github.com>
2018-08-14 16:54:42 +0530

    Update siri.py

--- siri.py
+++ siri.py
@@ -2,9 +2,9 @@ i=raw_input("")
 p=['a','e','i','o','u','A','E','I','O','U']
 if (i>='a' and i<='z'  or i>='A' and  i<='Z'):
        if (i in p):
-	print("vowel")
+	print("Vowel")
        else:
-        	print("consonant")
+        	print("Consonant")
 else:
 	print("invalid")
 	
